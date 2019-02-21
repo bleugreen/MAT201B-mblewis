ebe0dd1271f15c2186d9188c28d819fae6206d77
Mitchell Lewis <mitchell5799@gmail.com>
2019-02-21 12:09:21 -0800

    cleaned up

--- assignment_1/build/color_spaces/Release/CMakeFiles/color_spaces_run_debug.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/color_spaces_run_debug"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/color_spaces_run_debug.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- assignment_3/build/particles-p1/Release/CMakeFiles/particles-p1_run.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/particles-p1_run"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/particles-p1_run.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
