bfeab53c9cb63514dd780c699190ad076e59e351
Mitchell Lewis <mitchell5799@gmail.com>
2019-03-05 18:03:49 -0800

    optimized pillars, now only updates height instead of creating/destroying on each draw

--- final_project/distributedSketch1.cpp
+++ final_project/distributedSketch1.cpp
@@ -40,6 +40,8 @@ std::vector<int> bandWidths{ 2,2,2,2,2,3,3,3,3,4,4,5,6,7,8,10,12,15,19,23,28,38,
 std::vector<float> bandDist;
 std::vector<float> bandMax;
 
+std::vector<Mesh> pillars;
+
 struct SharedState {
     float soundVals[N];
 };
@@ -68,6 +70,8 @@ public:
             bandMax.push_back(0.1);
         }
         
+        createPillars();
+        
     }
 
     virtual void onDraw(Graphics &g) override {
@@ -80,66 +84,84 @@ public:
             
             //drawWaves(g, waves);
             
-            for(int i=0; i<N; i++){
-                float angle = i * (M_PI / N);
-                pillar(g, state().soundVals[i], angle);
-                pillar(g,state().soundVals[N-1-i], -angle);
-            }
+            drawPillars(g, state().soundVals);
         }
     }
     
     float radius = 4.0;
-    void pillar(Graphics& g, float height, float angle){
-        if(height == 0) return;
-        
-        float color = abs(angle/M_PI);
-        if(angle < 0) color = 1 - color;
-        
-        Vec3f bandCenter(radius*cos(angle), 0, radius*sin(angle));
-        Vec3f bandCross = bandCenter.cross(Vec3f(0, 1, 0));
-        
-        Vec3f vertexA = (bandCenter + bandCenter/10) - (bandCross/20);
-        Vec3f vertexB = (bandCenter + bandCenter/10) + (bandCross/20);
-        Vec3f vertexC = (bandCenter - bandCenter/10) - (bandCross/20);
-        Vec3f vertexD = (bandCenter - bandCenter/10) + (bandCross/20);
-        
-        Vec3f vertexE = vertexA + Vec3f(0, height*radius, 0);
-        Vec3f vertexF = vertexB + Vec3f(0, height*radius, 0);
-        Vec3f vertexG = vertexC + Vec3f(0, height*radius, 0);
-        Vec3f vertexH = vertexD + Vec3f(0, height*radius, 0);
-        
-        Mesh m{Mesh::TRIANGLE_STRIP};
-        m.vertex(vertexA); // 0
-        m.vertex(vertexE); // 1 -- vals that need to be modulated
-        m.vertex(vertexF); // 2 --
-        m.vertex(vertexB); // 3
-        m.vertex(vertexA); // 4
-        
-        m.vertex(vertexE); // 5 --
-        m.vertex(vertexG); // 6 --
-        m.vertex(vertexC); // 7
-        
-        m.vertex(vertexA); // 8
-        m.vertex(vertexB); // 9
-        m.vertex(vertexD); // 10
-        
-        m.vertex(vertexH); // 11 --
-        m.vertex(vertexF); // 12 --
-        m.vertex(vertexB); // 13
-        
-        m.vertex(vertexD); // 14
-        m.vertex(vertexH); // 15 --
-        m.vertex(vertexG); // 16 --
-        
-        m.vertex(vertexC); // 17
-        m.vertex(vertexD); // 18
-        m.vertex(vertexA); // 19
-        
-        
-        
-        g.color( HSV(color, abs(height), abs(height)) );
-        g.draw(m);
+    
+    void createPillars(){
+        for(int i=0; i<N; i++){
+            float angle = i * (M_PI / N);
+            float color = abs(angle/M_PI);
+            Vec3f bandCenter(radius*cos(angle), 0, radius*sin(angle));
+            Vec3f bandCross = bandCenter.cross(Vec3f(0, 1, 0));
+            
+            Vec3f vertexA = (bandCenter + bandCenter/10) - (bandCross/20);
+            Vec3f vertexB = (bandCenter + bandCenter/10) + (bandCross/20);
+            Vec3f vertexC = (bandCenter - bandCenter/10) - (bandCross/20);
+            Vec3f vertexD = (bandCenter - bandCenter/10) + (bandCross/20);
+            
+            Vec3f vertexE = vertexA + Vec3f(0, radius, 0);
+            Vec3f vertexF = vertexB + Vec3f(0, radius, 0);
+            Vec3f vertexG = vertexC + Vec3f(0, radius, 0);
+            Vec3f vertexH = vertexD + Vec3f(0, radius, 0);
+            
+            Mesh m{Mesh::TRIANGLE_STRIP};
+            m.vertex(vertexA); // 0
+            m.vertex(vertexE); // 1 -- vals that need to be modulated
+            m.vertex(vertexF); // 2 --
+            m.vertex(vertexB); // 3
+            m.vertex(vertexA); // 4
+            
+            m.vertex(vertexE); // 5 --
+            m.vertex(vertexG); // 6 --
+            m.vertex(vertexC); // 7
+            
+            m.vertex(vertexA); // 8
+            m.vertex(vertexB); // 9
+            m.vertex(vertexD); // 10
+            
+            m.vertex(vertexH); // 11 --
+            m.vertex(vertexF); // 12 --
+            m.vertex(vertexB); // 13
+            
+            m.vertex(vertexD); // 14
+            m.vertex(vertexH); // 15 --
+            m.vertex(vertexG); // 16 --
+            
+            m.vertex(vertexC); // 17
+            m.vertex(vertexD); // 18
+            m.vertex(vertexA); // 19
+            
+
+            
+            pillars.push_back(m);
+            
+        }
     }
+    
+    void drawPillars(Graphics& g, float soundVals[]){
+        for(int i=0; i<N; i++){
+            float color = abs(i / (M_PI * N));
+            float height = soundVals[i];
+            
+            // update height
+            pillars[i].vertices()[1].y = radius*height;
+            pillars[i].vertices()[2].y = radius*height;
+            pillars[i].vertices()[5].y = radius*height;
+            pillars[i].vertices()[6].y = radius*height;
+            pillars[i].vertices()[11].y = radius*height;
+            pillars[i].vertices()[12].y = radius*height;
+            pillars[i].vertices()[15].y = radius*height;
+            pillars[i].vertices()[16].y = radius*height;
+            
+            g.color( HSV(color, abs(height), abs(height)) );
+            g.draw(pillars[i]);
+            
+        }
+    }
+    
     
     virtual void onSound(AudioIOData &io) override {
         if (role() == ROLE_AUDIO || role() == ROLE_DESKTOP) {
